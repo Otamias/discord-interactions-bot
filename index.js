0e55aff674288afd77dedd614830842c85ced0ac
Otamias <92829769+Otamias@users.noreply.github.com>
2024-07-28 17:08:26 +0800

    index.js

--- index.js
+++ index.js
@@ -4,13 +4,49 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const contentMap = {
+    'example1': {
+        type: 'image',
+        url: 'https://example.com/picture1.jpg'
+    },
+    'example2': {
+        type: 'video',
+        url: 'https://example.com/video1.mp4'
+    }
+    // Add more mappings as needed
+};
+
 app.post('/api/interactions', (req, res) => {
     const interaction = req.body;
 
     if (interaction.type === 1) {
-        res.send({ type: 1 });
+        res.send({ type: 1 }); // Respond to PING
     } else if (interaction.type === 2) {
-        // Handle other interaction types
+        const command = interaction.data.name;
+        const keyword = command.split('_')[1];
+
+        if (keyword && contentMap[keyword]) {
+            const content = contentMap[keyword];
+            const response = {
+                type: 4,
+                data: {
+                    content: `Here is the ${content.type} you requested!`,
+                    embeds: content.type === 'image' ? [{
+                        image: { url: content.url }
+                    }] : [{
+                        video: { url: content.url }
+                    }]
+                }
+            };
+            res.send(response);
+        } else {
+            res.send({
+                type: 4,
+                data: {
+                    content: 'Sorry, I couldn\'t find that content.'
+                }
+            });
+        }
     }
 });
 
